c4abe949f774e7cc325ad8507e3ac1652f97802d
leontmp <leontmaria@yandex.ru>
2017-08-21 21:47:03 +0300

    Class Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
