bc6204e6bda7ce9e43963b5b2d5d62d2ec703467
leontmp <leontmaria@yandex.ru>
2017-08-22 15:15:43 +0300

    List Class

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -6,12 +7,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-           p1.Draw();
+            Point p1 = new Point(1, 3, '*');
+            Point p2 = new Point(2, 4, '#');
+            Point p3 = new Point(5, 6, '-');
+            Point p4 = new Point(8, 9, '/');
 
-            Point p2 = new Point(2,4,'#');
-            p2.Draw();
+            List<Point> list = new List<Point>();
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+            list.Add(p4);
 
+            foreach (Point i in list) {
+                i.Draw();
+            }
+            
             Console.ReadLine();
         }
     }
